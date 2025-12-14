a4dfa7c68301e54e4067f60b479ff0d6aeedd80f
Malte <91071750+Loxiuz@users.noreply.github.com>
2025-12-14 05:19:56 +0100

    Remove unnecessary auto-configuration exclusions and enable all data configurations

--- src/test/java/com/testing_exam_webapp/TestingExamWebappApplicationTests.java
+++ src/test/java/com/testing_exam_webapp/TestingExamWebappApplicationTests.java
@@ -1,11 +1,9 @@
 package com.testing_exam_webapp;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled("Disabled until further notice")
 class TestingExamWebappApplicationTests {
 
     @Test
